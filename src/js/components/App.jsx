f956ed75718d210c0990b837f9d7c4ade6ee2917
Jim <vic20087cjimlin@gmail.com>
2017-02-01 13:58:26 +0800

    add section style

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -9,14 +9,60 @@ export default CSSModules(class extends Component {
         return (
             <StyleRoot>
                 <Containers.Navbar/>
-                <div className="section">
-                    section 1
+                <div style={{
+                    height: '50px',
+                    width: '100%',
+                    display: 'block'
+                }}></div>
+                <div className="section lighten">
+                    <div className="sectionTitle">
+                        <h1>清大行動家</h1>
+                        <p></p>
+                    </div>
+                    <div className="sectionContent"></div>
                 </div>
-                <div className="section">
-                    section 2
+                <div className="section darken">
+                    <div className="sectionTitle">
+                        <h1>計畫簡介</h1>
+                        <p></p>
+                    </div>
+                    <div className="sectionContent"></div>
                 </div>
-                <div className="section">
-                    section 2
+                <div className="section lighten">
+                    <div className="sectionTitle">
+                        <h1>推薦名單</h1>
+                    </div>
+                    <div className="sectionContent"></div>
+                </div>
+                <div className="section darken">
+                    <div className="sectionTitle">
+                        <h1>計畫執行</h1>
+                    </div>
+                    <div className="sectionContent"></div>
+                </div>
+                <div className="section lighten">
+                    <div className="sectionTitle">
+                        <h1>推薦表單</h1>
+                    </div>
+                    <div className="sectionContent"></div>
+                </div>
+                <div className="section darken">
+                    <div className="sectionTitle">
+                        <h1>評審團隊</h1>
+                    </div>
+                    <div className="sectionContent"></div>
+                </div>
+                <div className="section lighten">
+                    <div className="sectionTitle">
+                        <h1>計畫時程</h1>
+                    </div>
+                    <div className="sectionContent"></div>
+                </div>
+                <div className="section darken">
+                    <div className="sectionTitle">
+                        <h1>主辦單位</h1>
+                    </div>
+                    <div className="sectionContent"></div>
                 </div>
                 { process.env.NODE_ENV !== 'production' ? <Containers.DevTools/> : null }
             </StyleRoot>
