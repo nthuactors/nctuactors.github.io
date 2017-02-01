ae8c38ae19e3754803b40490960c1f4a1425edd1
Tocknicsu <wingemerald@gmail.com>
2017-02-01 11:12:30 +0800

    add style

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import Containers from 'containers'
 import Radium, { StyleRoot } from 'radium'
+import CSSModules from 'react-css-modules'
 
 @Radium
-export default class extends Component {
+export default CSSModules(class extends Component {
     render () {
         return (
             <StyleRoot>
                 <Containers.Navbar/>
+                <div className="red">XD</div>
                 { process.env.NODE_ENV !== 'production' ? <Containers.DevTools/> : null }
             </StyleRoot>
         )
     }
-}
+}, require('./App.styl'))
