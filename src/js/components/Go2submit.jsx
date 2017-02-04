b31f9573cd6c71d094956fbec9b6a3c9b4b17426
Jim <vic20087cjimlin@gmail.com>
2017-02-05 01:52:55 +0800

    fix banner styl

--- src/js/components/Go2submit.jsx
+++ src/js/components/Go2submit.jsx
@@ -7,11 +7,14 @@ export default CSSModules(class extends Component {
             <div className="submitIntro">
                 <div className="submittext">
                     <p>
-                        我們想要找到努力實踐夢想之清華校友。如果你認識、或你自己就是這樣的行動者，歡迎提名或報名！
+                        我們想要找到努力實踐夢想之清華校友。
+                    </p>
+                    <p>
+                        如果你認識、或你自己就是這樣的行動者，歡迎提名或報名！
                     </p>
                 </div>
                 <div className="submitBtn">
-                    <a href="#">前往表單</a>
+                    <a href="#">我要推薦</a>
                 </div>
             </div>
         )
--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -24,7 +24,7 @@ export default CSSModules(class extends Component {
         const expand = {
             active: this.state.navExpand
         }
-        const navItem = ['清大行動家', '計畫簡介', '推薦名單', '計畫執行', '推薦表單', '評審團隊', '計畫時程', '主辦單位']
+        const navItem = ['清華築夢者', '計畫簡介', '推薦名單', '計畫執行', '推薦表單', '評審團隊', '計畫時程', '主辦單位']
         return (
             <nav id="nav" ref="nav">
                 <div className={classNames('menu-btn', 'nav-menu', expand)} onClick={this.toggleMenu}></div>
