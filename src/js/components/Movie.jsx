be7dc4bce306933701975286ffaf8afa98e74565
Tocknicsu <wingemerald@gmail.com>
2017-02-16 02:38:29 +0800

    optimize video code

--- src/js/components/Movie.jsx
+++ src/js/components/Movie.jsx
@@ -1,38 +1,16 @@
 import React, { Component } from 'react'
 import CSSModules from 'react-css-modules'
-import YouTube from 'react-youtube'
 
 export default CSSModules(class extends Component {
-    _onReady (event) {
-        event.target.mute()
-    }
     render () {
-        const opts = {
-            playerVars: {
-                autoplay: 1,
-                controls: 0,
-                autohide: 1,
-                loop: 1,
-                showinfo: 0,
-                modestbranding: 0,
-                playlist: 'VqbR0BrS-08',
-                disablekb: 1,
-                enable: 'jsapi',
-                origin: 'https://nthuactors.github.io/dist/'
-            }
-        }
-
         return (
             <div className="MovieContainers">
-                <div className="video-background">
-                    <div className="video-foreground">
-                        <YouTube
-                            videoId="VqbR0BrS-08"
-                            opts={opts}
-                            onReady={this._onReady}
-                            />
-                    </div>
-                </div>
+                <video autoPlay loop muted style={{
+                    width: '100vw',
+                    minHeight: 'calc(100vh-50px)'
+                }}>
+                    <source src="https://nthuactors.github.io/src/movie/MVI_2870.MP4" type="video/mp4" />
+                </video>
             </div>
         )
     }
