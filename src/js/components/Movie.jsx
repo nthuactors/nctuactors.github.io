289f9eecac8ccde115ba2f8b7d34deaa081a35c9
Jim <vic20087cjimlin@gmail.com>
2017-02-23 13:48:20 +0800

    add gif

--- src/js/components/Movie.jsx
+++ src/js/components/Movie.jsx
@@ -5,12 +5,12 @@ export default CSSModules(class extends Component {
     render () {
         return (
             <div className="MovieContainers">
-                <video autoPlay loop muted style={{
+                {/* <video autoPlay loop muted style={{
                     width: '100vw',
                     minHeight: 'calc(100vh-50px)'
                 }}>
                     <source src="https://nthuactors.github.io/src/movie/MVI_2870.MP4" type="video/mp4" />
-                </video>
+                </video> */}
             </div>
         )
     }
