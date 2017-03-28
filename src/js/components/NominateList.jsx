fbdfd4fb2309d8788bb568c6993db4e8d71c8ccb
Jim <vic20087cjimlin@gmail.com>
2017-03-28 14:25:06 +0800

    add new section content

--- src/js/components/NominateList.jsx
+++ src/js/components/NominateList.jsx
@@ -10,7 +10,25 @@ export default CSSModules(class extends Component {
         this.changeFilter = this.changeFilter.bind(this)
         this.showMore = this.showMore.bind(this)
         this.state = {
-            filter: 'first'
+            filter: 'first',
+            JudgeList: [
+                {
+                    'img_link': 'https://nthuactors.github.io/src/js/components/img/judge/j1.jpg',
+                    'name': '張鐵志',
+                    'mention': '政治與文化評論家',
+                    'mention2': '這樣的家庭問題，涉及的層面，既複雜，且嚴肅。用最簡單的一句話涵蓋，就是世代落差，導致世代隔閡，進而世代對立，最後不可收拾。但是若回歸到原始的起點，如果上一代願意多聆聽，下一代願意多理解，就不至於讓錯誤的步伐一直走到難以轉圜，最後造成雙方的遺憾。'
+                }, {
+                    'img_link': 'https://nthuactors.github.io/src/js/components/img/judge/j2.jpg',
+                    'name': '劉月梅',
+                    'mention': '荒野保護協會理事長',
+                    'mention2': '這樣的家庭問題，涉及的層面，既複雜，且嚴肅。用最簡單的一句話涵蓋，就是世代落差，導致世代隔閡，進而世代對立，最後不可收拾。但是若回歸到原始的起點，如果上一代願意多聆聽，下一代願意多理解，就不至於讓錯誤的步伐一直走到難以轉圜，最後造成雙方的遺憾。'
+                }, {
+                    'img_link': 'https://nthuactors.github.io/src/js/components/img/judge/j3.jpg',
+                    'name': '史欽泰',
+                    'mention': '前清大科管院院長',
+                    'mention2': '這樣的家庭問題，涉及的層面，既複雜，且嚴肅。用最簡單的一句話涵蓋，就是世代落差，導致世代隔閡，進而世代對立，最後不可收拾。但是若回歸到原始的起點，如果上一代願意多聆聽，下一代願意多理解，就不至於讓錯誤的步伐一直走到難以轉圜，最後造成雙方的遺憾。'
+                }
+            ]
         }
     }
     changeFilter (filter) {
@@ -36,12 +54,12 @@ export default CSSModules(class extends Component {
             <div className="NominateContainer">
                 <div className="filterBox">
                     <div onClick={() => { this.changeFilter('first') } }>推薦名單</div>
-                    <div onClick={() => { this.changeFilter('last') } }>最終名單</div>
+                    <div onClick={() => { this.changeFilter('last') } }>評審的話</div>
                 </div>
                 <div className="peopleBox">
                     <ul>
-                    {
-                        this.props.NominateList.map((ele, id) => (
+                    {this.state.filter === 'first'
+                        ? this.props.NominateList.map((ele, id) => (
                                 ele[this.state.filter]
                                 ? <li key={id} >
                                     <div className="listContain" onClick={() => { this.showMore(id) } }>
@@ -63,6 +81,24 @@ export default CSSModules(class extends Component {
                                 </li> : null
                             )
                         )
+                    : <div className='judgeWords'>
+                        {
+                            this.state.JudgeList.map((ele, id) => (
+                                <li>
+                                    <div className="judgeIntro">
+                                        <div className="judgeWrapper">
+                                            <img src={ele.img_link}/>
+                                        </div>
+                                        <h3>{ele.name}</h3>
+                                        <p>{ele.mention}</p>
+                                    </div>
+                                    <div className="judgeWord">
+                                        <p> {ele.mention2} </p>
+                                    </div>
+                                </li>
+                            ))
+                        }
+                    </div>
                     }
                     </ul>
                 </div>
